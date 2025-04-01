fa9982b28061416e2e9445ea264a9f1507d52392
ludovici96 <ludovici96@users.noreply.github.com>
2025-04-02 01:23:27 +0200

    Initialize solution and add StringLibrary, ShowCase, and StringLibrary.Tests projects

---
+++ ClassLibraryProjects/StringLibrary.Tests/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using Xunit;
+using UtilityLibraries;
+
+namespace StringLibrary.Tests
+{
+    public class StringLibraryTests
+    {
+        [Theory]
+        [InlineData("Alphabet", true)]
+        [InlineData("zebra", false)]
+        [InlineData(" ", false)]
+        [InlineData("", false)]
+        [InlineData(null, false)]
+        public void StartsWithUpper_ReturnsExpectedResults(string input, bool expected)
+        {
+            bool result = input.StartsWithUpper();
+            Assert.Equal(expected, result);
+        }
+    }
+}
